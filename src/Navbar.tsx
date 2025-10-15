32266bf1a12c1617f74c9b3257ecc4c216a20758
Stan <taix97@outlook.com>
2025-10-15 15:27:59 +0200

    improves

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -5,10 +5,10 @@ const Navbar: React.FC = () => {
   return (
     <nav className="navbar">
       <ul className="nav-list">
-        <li><a className="nav-link" href="/index.html#home">Home</a></li>
+        <li><a className="nav-link" href="/index.html">Home</a></li>
         <li><a className="nav-link" href="/about.html">About</a></li>
-        <li><a className="nav-link" href="/index.html#projects">Projects</a></li>
-        <li><a className="nav-link" href="/index.html#contact">Contact</a></li>
+        <li><a className="nav-link" href="/projects">Projects</a></li>
+        <li><a className="nav-link" href="/contact">Contact</a></li>
       </ul>
     </nav>
   );
